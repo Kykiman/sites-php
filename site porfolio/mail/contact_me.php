4fb08363bf66966b024a02fbf6092ed53c38a57a
Kykiman <kykiman2000@gmail.com>
2018-05-07 14:30:53 +0200

    1

--- site porfolio/mail/contact_me.php
+++ site porfolio/mail/contact_me.php
@@ -1,5 +1,4 @@
 <?php
-// Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['message'])	||
@@ -13,11 +12,11 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 
-// Create the email and send the message
+
 $to = 'kylian.manzini@cpnv.ch';
 $email_subject = "Contact depuis le porfolio";
 $email_body = "Contact de $name.\n\n"."Son Email: $email_address\n\nSon Message:\n$message";
-$headers = "From: kmanzini@mediamatique.ch\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: kmanzini@mediamatique.ch\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
